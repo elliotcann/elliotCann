9a86703b20337026e0cda2429a58f6f2704afa53
Elliot Cann <elliotcann@gmail.com>
2025-01-13 21:22:40 +0000

    Correct line 8 and line 25 of php file and add error message to js file

--- task/libs/js/script.js
+++ task/libs/js/script.js
@@ -2,24 +2,28 @@
 $('#oceanSubmit').click(function() {
 
     $.ajax({
-        url: "libs/php/getOceanInfo.php",
+        url: "php/getOceanInfo.php",
         type: 'POST',
         dataType: 'json',
         data: {
-            lat: $('#lat').val(),
-            lng: $('#lng').val()
+            lat: $('#oceanLat').val(),
+            lng: $('#oceanLng').val()
         },
         success: function(result) {
 
-            console.log(result);
+            console.log(JSON.stringify(result));
 
             if (result.status.name == "ok") {
-                $('#oceanName').html(result.data.ocean.name);
+                $('#oceanName').html(result['ocean']['name']);
             }
         
         },
         error: function(jqXHR, textStatus, errorThrown) {
             // your error code
+            alert( "Sorry, there was a problem!" );
+            console.log( "Error: " + errorThrown );
+            console.log( "Status: " + textStatus );
+            console.dir( jqXHR );
         }
     }); 
 
