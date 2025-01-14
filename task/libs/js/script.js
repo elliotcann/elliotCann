700fbe79f29031c36a9330aa002d0a679c1a0509
Elliot Cann <elliotcann@gmail.com>
2025-01-14 12:05:50 +0000

    Add js script to js file for earthquakes

--- task/libs/js/script.js
+++ task/libs/js/script.js
@@ -29,4 +29,36 @@ $('#oceanSubmit').click(function() {
 
 });
 
-/* JS script for Earthquakes */
+/* JS script for Earthquake */
+$('#eqSubmit').click(function() {
+
+    $.ajax({
+        url: "libs/php/getEarthquakeInfo.php",
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            north: $('#eqNorth').val(),
+            south: $('#eqSouth').val(),
+            east: $('#eqEast').val(),
+            west: $('#eqWest').val()
+        },
+        success: function(result) {
+
+            console.log(JSON.stringify(result));
+
+            if (result.status.name == "ok") {
+                $('#eqDateTime').html(result['data'][0]['datetime']);
+                $('#eqSrc').html(result['data'][0]['src']);
+                $('#eqMagnitude').html(result['data'][0]['magnitude']);
+            }
+        
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            alert( "No earthquakes within these coordinates!" );
+            console.log( "Error: " + errorThrown );
+            console.log( "Status: " + textStatus );
+            console.dir( jqXHR );
+        }
+    }); 
+
+});
